a8102a31d42656a8439b3093acbd49de98edc012
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-12 16:00:22 +0300

    added TabBar
    создал TabBar

---
+++ PRORecipies/PRORecipies/Views/Navigation/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  PRORecipies
+//
+//  Created by Anvar on 11.04.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ PRORecipies/PRORecipies/Views/Navigation/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  PRORecipies
+//
+//  Created by Anvar on 11.04.2023.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
