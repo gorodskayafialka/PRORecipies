dcdc1e368257fdc808188fbf096281c1cbd17f7a
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-12 16:00:22 +0300

    added TabBar
    создал TabBar

---
+++ PRORecipies/PRORecipies/Models/UI/TabItem.swift
@@ -0,0 +1,58 @@
+//
+//  TabItem.swift
+//  PRORecipies
+//
+//  Created by Anvar on 11.04.2023.
+//
+
+import SwiftUI
+
+struct TabItem: Identifiable, Hashable, Equatable {
+    let id = UUID()
+    var name: String
+    var icon: String
+    var color: Color
+    var selection: Tab
+}
+
+enum Tab: String {
+    case home = "Home"
+    case explore = "Explore"
+    case favorites = "Favorites"
+    case list = "List"
+}
+
+extension Tab {
+    var tabItem: TabItem {
+        TabItem(name: name, icon: icon.rawValue, color: color, selection: selection)
+    }
+
+    private var name: String {
+        self.rawValue
+    }
+
+    private var icon: Icons {
+        switch self {
+        case .home:
+            return Icons.home
+        case .explore:
+            return Icons.magnifyingglass
+        case .favorites:
+            return Icons.heart
+        case .list:
+            return Icons.list
+        }
+    }
+
+    private var color: Color {
+        Color.blue
+    }
+
+    private var selection: Tab {
+        self
+    }
+}
+
+extension Tab: CaseIterable {
+
+}
