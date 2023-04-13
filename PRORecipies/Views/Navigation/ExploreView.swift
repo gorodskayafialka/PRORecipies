ab66fbb112d47b3ce162c367fb674d3701e2105f
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-13 15:14:30 +0300

    tabView fix

--- PRORecipies/Views/Navigation/ExploreView.swift
+++ PRORecipies/Views/Navigation/ExploreView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ExploreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.blue
+
+            Text("ExploreView")
+        }
     }
 }
 
