d79aa70b83a39fff9104a66df8516cad97891df9
yaanvar <54923760+yaanvar@users.noreply.github.com>
2023-04-13 15:14:30 +0300

    tabView fix

--- PRORecipies/Views/Navigation/FavoritesView.swift
+++ PRORecipies/Views/Navigation/FavoritesView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct FavoritesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.green
+
+            Text("FavoritesView")
+        }
     }
 }
 
--- PRORecipies/Views/Navigation/HomeView.swift
+++ PRORecipies/Views/Navigation/HomeView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.red
+
+            Text("HomeView")
+        }
     }
 }
 
