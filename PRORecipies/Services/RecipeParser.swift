fb73bd2659d3dac596967fa0891de9d462801a12
denchicez <denchicez@gmail.com>
2023-04-18 17:58:31 +0300

    rm init

--- PRORecipies/Services/RecipeParser.swift
+++ PRORecipies/Services/RecipeParser.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum InstructionsParser {
     static func parse(instruction: String) -> [String] {
-        let arrayWithOutStep = instruction.split(separator: /\s*(S|s)(T|t)(E|e)(P|p)\s*\d*\s*/).map({String($0)})
-        return arrayWithOutStep.joined(separator: " ").split(separator: /\.\s*/).map({String($0)})
+        let arrayWithOutStep = instruction.split(separator: /\s*(S|s)(T|t)(E|e)(P|p)\s*\d*\s*/).map(String.init)
+        return arrayWithOutStep.joined(separator: " ").split(separator: /\.\s*/).map(String.init)
     }
 }
